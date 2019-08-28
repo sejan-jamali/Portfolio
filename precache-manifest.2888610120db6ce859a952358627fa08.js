30e3d29e12a5933df999aaba492bcc82efdf5937
sejan-jamali <sifathaiderjamali@gmail.com>
2019-08-29 00:59:07 +0600

    Updates

---
+++ precache-manifest.2888610120db6ce859a952358627fa08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "591eb5beb99e69ed8d3e",
+    "url": "/Portfolio/static/js/runtime~main.8760695a.js"
+  },
+  {
+    "revision": "f9553f9efdd720a4903a",
+    "url": "/Portfolio/static/js/main.a99e4a22.chunk.js"
+  },
+  {
+    "revision": "29a48359912a5db57472",
+    "url": "/Portfolio/static/js/2.6857d3f7.chunk.js"
+  },
+  {
+    "revision": "f9553f9efdd720a4903a",
+    "url": "/Portfolio/static/css/main.7725e53b.chunk.css"
+  },
+  {
+    "revision": "bfbcbd786394050d05c016b97cc5f19b",
+    "url": "/Portfolio/index.html"
+  }
+];
