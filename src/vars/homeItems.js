b2a88f451c3d9173a0d4b50d5c06872898bd3a30
sejan-jamali <sifathaiderjamali@gmail.com>
2019-08-29 21:27:49 +0600

    update

--- src/vars/homeItems.js
+++ src/vars/homeItems.js
@@ -1,7 +1,7 @@
 var myName = 'S.M Sifat Haider Jamali'
 var myOccupation = 'Software Developer'
 var myDetails = 'Hello, I am Sifat Haider Jamali. I love Programming, making application/software for Mobile, Web and Desktop. I have a professional certificate in Dot Net Development.\nI I love work in OOP. I learned C, C++, Java, C#, Python, JavaScript, PHP. Worked in so many libraries & frameworks like .Net(one of my favorites), Laravel, worked using technologies like Git, Arduino, etc. Always forward to improve myself. One of my favorite quote is "earn to learn"'
-var myImageLink = "https://i.imgur.com/5t7rVR4.png"
+var myImageLink = "https://i.imgur.com/aXfxvBy.jpg?1"
 
 module.exports = {
     myName, myOccupation, myDetails, myImageLink
